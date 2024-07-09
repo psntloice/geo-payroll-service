21d19c8f240bcb530ef937c3e8f703a4a1a5d1c6
unknown <nyandiakamau24@gmail.com>
2024-07-09 05:05:26 +0300

    vercel

--- bd/payroll-service/routes/api.php
+++ bd/payroll-service/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PayrollController;
 use App\Http\Controllers\PayPeriodController;
 use App\Http\Controllers\EarningController;
 use App\Http\Controllers\DeductionController;
+
     Route::resource('payPeriods', PayPeriodController::class);
     Route::resource('earnings', EarningController::class);
     Route::resource('deductions', DeductionController::class);
